33441fac759f29995cc5b364fc1fb3cbc75b1975
Gabriel <gabri8989ramos.sk9@gmail.com>
2024-04-26 13:23:31 -0300

    docs:Component Concepts

--- dio/src/App.js
+++ dio/src/App.js
@@ -3,9 +3,9 @@ function App() {
   return (
   <div className="App"> <h1>Olá React</h1>
   <Button title ='Entrar'/>
-  <Button title ='Entrar'/>
-  <Button title ='Entrar'/>
-  <Button title ='Entrar'/>
+  <Button title ='Fechar'/>
+  <Button title ='Abrir'/>
+  <Button title ='Salvar'/>
   </div>);
 }
 

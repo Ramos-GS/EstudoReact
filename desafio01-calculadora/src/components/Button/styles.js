d7e0102abf6eac3b0e9a2a0f48aed8a13d017097
Gabriel <gabri8989ramos.sk9@gmail.com>
2024-05-02 13:51:07 -0300

    style:adding the hover to the buttons

--- desafio01-calculadora/src/components/Button/styles.js
+++ desafio01-calculadora/src/components/Button/styles.js
@@ -1,15 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export  const ButtonContainer =  styled.button`
-    padding:20px;
-    border: 1px solid #CDCDCD;
-    background-color:#00AAFD;
-    color: #FFFFFF;
-    font-size: 24px;
-    font-weight:700;
-    flex:1;
-    border-radius: 5px;
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #cdcdcd;
+  background-color: #00aafd;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+  border-radius: 5px;
 
-
-
-`
+  &:hover {
+        opacity: 0.6;
+    }
+`;

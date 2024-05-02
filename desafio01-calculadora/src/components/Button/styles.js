1aba953541cbc3eadeafdfb57d95a18a02dfb7da
Gabriel <gabri8989ramos.sk9@gmail.com>
2024-05-02 13:25:22 -0300

    feat:creating buttons and their styles

--- desafio01-calculadora/src/components/Button/styles.js
+++ desafio01-calculadora/src/components/Button/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 export  const ButtonContainer =  styled.button`
-    padding:10px;
+    padding:20px;
     border: 1px solid #CDCDCD;
     background-color:#00AAFD;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight:700;
+    flex:1;
 
 
 
